34d931cbbf9179ceda9200b2e7694dfbb305a1c5
mjaliz <mrph14@gmail.com>
2024-08-10 11:57:37 +0330

    Optional interface properties

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ import { User } from "./models/User";
 const user = new User({ name: "Mj", age: 27 });
 console.log(user.get("name"));
 console.log(user.get("age"));
+user.set({ name: "Mohammad" });
+console.log(user);
